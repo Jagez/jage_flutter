27fb45fc15ebfe1e8954ac14073bc60ec57587e9
CLANNAD <lwk283587151@outlook.com>
2021-08-02 00:29:28 +0800

    Expanded组件使用
    对元素的扩展、拉伸组件，与Row/Column一起使用

--- lib/FirstComponent.dart
+++ lib/FirstComponent.dart
@@ -25,17 +25,56 @@ class FirstComponent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 500.0,
-      width: 400.0,
-      color: Colors.yellow,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,     //Y轴上的布局
-        crossAxisAlignment: CrossAxisAlignment.start,         //X轴上的布局
+      width: 750.0,
+      color: Colors.orange,
+      child: Row(
+        //mainAxisAlignment: MainAxisAlignment.spaceEvenly,     //Y轴上的布局
+        //crossAxisAlignment: CrossAxisAlignment.start,         //X轴上的布局
         children: [
-          Image.asset("images/icon_quest_31075.png"),
-          Image.asset("images/icon_quest_31075.png"),
-          Image.asset("images/icon_quest_31075.png"),
-          Image.asset("images/icon_quest_31075.png"),
+          Expanded(
+            flex: 1,
+            child: IconContainer(
+              Icons.search,
+              color: Colors.blue,
+              size: 32,
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: IconContainer(
+              Icons.file_copy_sharp,
+              size: 32,
+            ),
+          ),
+          IconContainer(
+            Icons.home,
+            color: Colors.red,
+            size: 32,
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class IconContainer extends StatelessWidget {
+  IconData _iconData;
+  double? size = 32.0;
+  Color? color = Colors.yellow;
+
+  IconContainer(this._iconData, {this.color, this.size}) {}
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      height: 128.0,
+      width: 128.0,
+      color: this.color,
+      child: Center(
+        child: Icon(
+          this._iconData,
+          size: this.size,
+        ),
       ),
     );
   }
