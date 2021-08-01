5bd8c2467febd482a7d855d1fa6eddfd800a6f53
CLANNAD <lwk283587151@outlook.com>
2021-08-01 21:35:26 +0800

    GridView网格列表-builder
    GridView.builder方式创建网格列表

--- lib/FirstComponent.dart
+++ lib/FirstComponent.dart
@@ -3,39 +3,36 @@ import 'res/ListData.dart';
 import 'res/ListImages.dart';
 
 class FirstComponent extends StatelessWidget {
-
-  List<Widget> _getInfo() {
-    
-    var data = listImage.map((value) {
-        return Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Color.fromRGBO(233, 233, 233, 0.7),
-            ),
-          ),
-          child: Column(
-            children: [
-              Image.asset(value['uri']),
-              Text(
-                value['title']
-              ),
-            ],
-          ),
-        );
-      });
-      return data.toList();
-    
+  Widget _getInfo(content, index) {
+    return Container(
+      padding: EdgeInsets.all(5.0),
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 2.0,
+          color: Color.fromRGBO(233, 233, 233, 0.7),
+        ),
+      ),
+      child: Column(
+        children: [
+          Image.asset(listImage[index]['uri']),
+          Text(listImage[index]['title']),
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      padding: EdgeInsets.all(15.0),
-      crossAxisSpacing: 15.0, //水平间距
-      mainAxisSpacing: 15.0,  //垂直间距
-      crossAxisCount: 2,
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisSpacing: 15.0, //水平间距
+        mainAxisSpacing: 15.0, //垂直间距
+        crossAxisCount: 2,
+      ),
+      // padding: EdgeInsets.all(15.0),
       //childAspectRatio: 0.7,  //宽/高比例
-      children: this._getInfo(),
-      );
+      itemCount: listImage.length,
+      itemBuilder: this._getInfo,
+    );
   }
 }
