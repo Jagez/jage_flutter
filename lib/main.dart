2ebde8bde13e36218f1a60da9032cb59961cff9a
CLANNAD <lwk283587151@outlook.com>
2021-08-03 11:59:13 +0800

    Scaffold属性bottomNavigationBar实现分页
    将MaterialApp单独作为一个组件分离出去，添加三个页面组件，实现动态切换页面

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 //import 'FirstComponent.dart';
-import 'HomePage.dart';
+import 'tabs/Tabs.dart';
 void main() {
-  runApp(HomePage());
+  runApp(Tabs());
 }
---
+++ lib/tabs/Tabs.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'page/HomePage.dart';
+import 'page/Setting.dart';
+import 'page/Category.dart';
+class Tabs extends StatefulWidget {
+  Tabs({Key? key}) : super(key: key);
+
+  @override
+  _TabsState createState() => _TabsState();
+}
+
+class _TabsState extends State<Tabs> {
+  int _bottomNavIndex = 0;
+  List<Widget> _tabsList = List.empty(growable: true);
+  @override
+  Widget build(BuildContext context) {
+    this._tabsList.add(HomePage());
+    this._tabsList.add(Category());
+    this._tabsList.add(Setting());
+    return Container(
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              "Jage Flutter",
+            ),
+          ),
+          body: this._tabsList[this._bottomNavIndex],
+          bottomNavigationBar: BottomNavigationBar(
+            fixedColor: Theme.of(context).accentColor,
+            currentIndex: this._bottomNavIndex,
+            onTap: (int index) {
+              setState(() {
+                this._bottomNavIndex = index;
+
+              });
+            },
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: "Home",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.category),
+                label: "Category",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                label: "Settings",
+              ),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
