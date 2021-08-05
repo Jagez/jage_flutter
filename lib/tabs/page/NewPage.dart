c0d885ed1ddfea65a69d9dec36950843a3fc9044
CLANNAD <lwk283587151@outlook.com>
2021-08-05 14:59:33 +0800

    Flutter自定义appBar补充- TabBarView
    注意TabBar中的length长度与Tab和TabBarView的children个数要一致

--- lib/tabs/page/NewPage.dart
+++ lib/tabs/page/NewPage.dart
@@ -17,48 +17,57 @@ class _NewPageState extends State<NewPage> {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-      appBar: AppBar(
-        title: Text("NewPage"),
-        bottom: TabBar(
-          isScrollable: true,                         //Tab比较多时允许滚动
-          labelColor: Colors.yellowAccent,
-          indicatorSize: TabBarIndicatorSize.label,
-          indicatorColor: Colors.yellowAccent,
-          unselectedLabelColor: Colors.white,
-          tabs: [
-            Tab(text: "Hot",),
-            Tab(text: "Pop",),
+        appBar: AppBar(
+          title: Text("NewPage"),
+          bottom: TabBar(
+            isScrollable: true, //Tab比较多时允许滚动
+            labelColor: Colors.yellowAccent,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorColor: Colors.yellowAccent,
+            unselectedLabelColor: Colors.white,
+            tabs: [
+              Tab(
+                text: "Hot",
+              ),
+              Tab(
+                text: "Pop",
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Center(
+              child: Badge(
+                badgeContent: Text(widget.arguments['name']),
+                child: Image.asset("images/icon_quest_31020.png"),
+              ),
+            ),
+            Text("tab 2"),
           ],
         ),
-      ),
-      body: Center(
-        child: Badge(
-          badgeContent: Text(widget.arguments['name']),
-          child: Image.asset("images/icon_quest_31020.png"),
+        floatingActionButton: Badge(
+          badgeContent: Text(
+            this.content,
+            style: TextStyle(fontSize: 18.0),
+          ),
+          child: FloatingActionButton(
+            child: Text("Home"),
+            onPressed: () {
+              //返回根路由
+              Navigator.of(context).pushAndRemoveUntil(
+                  MaterialPageRoute(
+                    builder: (context) => new Tabs(),
+                  ),
+                  //将路由清空
+                  (route) => false);
+              setState(() {
+                this.content = widget.arguments['name'].toString();
+              });
+            },
+          ),
         ),
       ),
-      floatingActionButton: Badge(
-        badgeContent: Text(
-          this.content,
-          style: TextStyle(fontSize: 18.0),
-        ),
-        child: FloatingActionButton(
-          child: Text("Home"),
-          onPressed: () {
-            //返回根路由
-            Navigator.of(context).pushAndRemoveUntil(
-                MaterialPageRoute(
-                  builder: (context) => new Tabs(),
-                ),
-                //将路由清空
-                (route) => false);
-            setState(() {
-              this.content = widget.arguments['name'].toString();
-            });
-          },
-        ),
-      ),
-    ),
     );
   }
 }
